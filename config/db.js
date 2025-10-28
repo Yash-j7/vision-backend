604dfc8997549b81c7953c70268d8b5a97e226ec
Soyel Rana <75212971+SoyelRana07@users.noreply.github.com>
2025-10-28 20:47:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const connectDb = async () => {
   try {
     console.log("Connecting to MongoDB...");
     const conn = await mongoose.connect(process.env.MONGODB_URL, {
-      serverSelectionTimeoutMS: 5000, // fail fast if DB is not reachable
+      serverSelectionTimeoutMS: 5000, // fail fast if not reachable
     });
-    console.log(`Connected to MongoDB at ${conn.connection.host}`);
+    console.log(`Connected to MongoDB: ${conn.connection.host}`);
     return conn;
   } catch (error) {
     console.error("Database connection error:", error.message);
